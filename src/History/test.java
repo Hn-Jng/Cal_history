8b4ce0b6bee6c8ea43b334c9d01274a9d59650b7
Hn-Jng <61318633+Hn-Jng@users.noreply.github.com>
2020-06-25 19:08:29 +0900

    수정

--- src/History/test.java
+++ src/History/test.java
@@ -1,10 +1,28 @@
 package History;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class test {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		response.setContentType("text");
+		   PrintWriter out = response.getWriter();
+	       try {
+	           FileReader logReader = new FileReader("D:/out.txt");
+	           try {
+	               BufferedReader buffer = new BufferedReader(logReader);
+	               for (String line = buffer.readLine(); line != null; line = buffer.readLine()) {
+	                   out.println(line);
+	               }
+	           } finally {
+	               logReader.close();
+	           }
+	       } finally {
+	           out.close();
+	       }
 	}
 
 }
--- src/History/test2.java
+++ src/History/test2.java
@@ -1,10 +1,25 @@
 package History;
 
+import java.io.*;
+
+
 public class test2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		
+		String fileName = "D:/out.txt";
+		DataInputStream reader = new DataInputStream(new FileInputStream(fileName));
+		byte[] bytes = new byte[10];
+		reader.read(bytes);
+		String content = new String(bytes);
+		reader.close();
+
+		System.out.println(content);
+		
+	
+		}
+
 
 	}
 
-}
+
